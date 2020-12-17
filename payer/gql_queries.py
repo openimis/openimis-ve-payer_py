e5749c05b66737223833afef42ac82d36ef96b17
Eric Darchis <darchis@pobox.com>
2020-12-17 11:29:11 +0100

    Allow querying on payers

--- payer/gql_queries.py
+++ payer/gql_queries.py
@@ -7,8 +7,10 @@ from core import prefix_filterset, filter_validity, ExtendedConnection
 class PayerGQLType(DjangoObjectType):
     class Meta:
         model = Payer
+        interfaces = (graphene.relay.Node,)
         filter_fields = {
             "id": ["exact"],
             "uuid": ["exact"],
             "name": ["exact", "icontains"],
         }
+        connection_class = ExtendedConnection
