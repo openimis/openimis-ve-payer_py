99704ea75545699bc3c98de8620de4ac3ceeee14
Quentin Gérôme <geromequentin@gmail.com>
2021-10-07 13:46:22 +0000

    feat: Migrate Payers to Django

--- payer/schema.py
+++ payer/schema.py
@@ -1,39 +1,61 @@
+import graphene
 from django.db.models import Q
 from django.core.exceptions import PermissionDenied
-from graphene_django.filter import DjangoFilterConnectionField
 import graphene_django_optimizer as gql_optimizer
-
-from django.utils.translation import gettext as _
-from location.apps import LocationConfig
+from core import filter_validity
+from django.utils.translation import gettext as _, gettext_lazy
+from payer.apps import PayerConfig
 from core.schema import OrderedDjangoFilterConnectionField
+from .models import Payer
 
-# We do need all queries and mutations in the namespace here.
-from .gql_queries import *  # lgtm [py/polluting-import]
-from .gql_mutations import *  # lgtm [py/polluting-import]
+from .gql_queries import PayerGQLType
+
+from .gql_mutations import (
+    CreatePayerMutation,
+    UpdatePayerMutation,
+    DeletePayerMutation,
+    AddFundingMutation,
+)
+
 
 class Query(graphene.ObjectType):
     payers = OrderedDjangoFilterConnectionField(
         PayerGQLType,
-        parent_location=graphene.String(),
-        parent_location_level=graphene.Int(),
+        show_history=graphene.Boolean(),
+        search=graphene.String(
+            description=gettext_lazy("Search in `name`, `phone` & `email`")
+        ),
         orderBy=graphene.List(of_type=graphene.String),
     )
+    payer = graphene.Field(PayerGQLType, uuid=graphene.UUID())
+
+    def resolve_payer(self, info, uuid, **kwargs):
+        if not info.context.user.has_perms(PayerConfig.gql_query_payers_perms):
+            raise PermissionDenied(_("unauthorized"))
+
+        return Payer.objects.get(uuid=uuid)
 
     def resolve_payers(self, info, **kwargs):
-        # if not info.context.user.has_perms(PayerConfig.gql_query_payers_perms):
-        #     raise PermissionDenied(_("unauthorized"))
+        if not info.context.user.has_perms(PayerConfig.gql_query_payers_perms):
+            raise PermissionDenied(_("unauthorized"))
         filters = []
-        # show_history = kwargs.get('show_history', False)
-        # if not show_history and not kwargs.get('uuid', None):
-        #     filters += filter_validity(**kwargs)
-        parent_location = kwargs.get('parent_location')
-        if parent_location is not None:
-            parent_location_level = kwargs.get('parent_location_level')
-            if parent_location_level is None:
-                raise NotImplementedError("Missing parentLocationLevel argument when filtering on parentLocation")
-            f = "uuid"
-            for i in range(len(LocationConfig.location_types) - parent_location_level - 1):
-                f = "parent__" + f
-            payer_location = "location__" + f
-            filters.append(Q(**{payer_location: parent_location}))
-        return gql_optimizer.query(Payer.objects.filter(*filters).all(), info)
+        show_history = kwargs.get("show_history", False)
+        if not show_history:
+            filters += filter_validity(**kwargs)
+
+        search = kwargs.get("search", None)
+        if search is not None:
+            filters.append(
+                Q(name__icontains=search)
+                | Q(phone__icontains=search)
+                | Q(email__icontains=search)
+            )
+
+        return gql_optimizer.query(Payer.objects.filter(*filters), info)
+
+
+class Mutation(graphene.ObjectType):
+    create_payer = CreatePayerMutation.Field()
+    update_payer = UpdatePayerMutation.Field()
+    delete_payer = DeletePayerMutation.Field()
+    add_funding = AddFundingMutation.Field()
