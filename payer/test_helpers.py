a2d5f7f14918e1361a5c9340ff4c148f7006b4ab
delcroip <patrick.delcroix@swisstph.ch>
2024-09-03 15:47:29 +0200

    better test helpers

--- payer/test_helpers.py
+++ payer/test_helpers.py
@@ -4,6 +4,10 @@ from contribution.models import Payer, Premium
 
 
 def create_test_payer(payer_type=Payer.PAYER_TYPE_OTHER, custom_props=None):
+    if custom_props is None:
+        custom_props = {}
+    else:
+        custom_props = {k: v for k, v in custom_props.items() if hasattr(Payer, k)}
     payer = Payer.objects.create(
         **{
             "type": payer_type,
@@ -12,7 +16,7 @@ def create_test_payer(payer_type=Payer.PAYER_TYPE_OTHER, custom_props=None):
             "address": "Test street name 123, CZ9204 City, Country",
             "validity_from": "2019-01-01",
             "audit_user_id": -1,
-            **(custom_props if custom_props else {})
+            **custom_props
         }
     )
     return payer
